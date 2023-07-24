83fa37d3de0c05e60dce7a4a1278c2c75d26b768
Jose A. Rivera <jarrpa@redhat.com>
2023-07-24 09:36:45 -0500

    reporter: submit and ack reports against mock api

--- cmd/metering-reporter/main.go
+++ cmd/metering-reporter/main.go
@@ -17,8 +17,14 @@ limitations under the License.
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+
+	cdr "github.com/red-hat-storage/managed-fusion-metering/mock/api/v1"
 
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -44,7 +50,116 @@ func init() {
 	//+kubebuilder:scaffold:scheme
 }
 
+func GetToken(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func SendConsumptionReport(url string, report cdr.Request) ([]byte, error) {
+	payload, err := json.Marshal(report)
+	if err != nil {
+		return nil, err
+	}
+	debug(string(payload))
+
+	payloadBuffer := bytes.NewBuffer(payload)
+	resp, err := http.Post(url, "application/json", payloadBuffer)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func GetAcknowledgement(url string, report cdr.Request) ([]byte, error) {
+	payload, err := json.Marshal(report)
+	if err != nil {
+		return nil, err
+	}
+	debug(string(payload))
+
+	payloadBuffer := bytes.NewBuffer(payload)
+	resp, err := http.Post(url, "application/json", payloadBuffer)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
 func main() {
+	cdrServiceHost := os.Getenv("MOCK_CDR_SERVICE_SERVICE_HOST")
+	cdrServicePort := os.Getenv("MOCK_CDR_SERVICE_SERVICE_PORT")
+	cdrEndpoint := "consumption"
+	cdrServiceUrl := "http://" + cdrServiceHost + ":" + cdrServicePort + "/" + cdrEndpoint
+	log.Println("MOCK_CDR_SERVICE_SERVICE_HOST: " + cdrServiceHost)
+	log.Println("MOCK_CDR_SERVICE_SERVICE_PORT: " + cdrServicePort)
+
+	tokenBody, err := GetToken(cdrServiceUrl)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	debug(string(tokenBody))
+
+	cdrReport := cdr.Request{
+		Action: "create",
+		DataSet: cdr.DataSet{
+			Results: []cdr.Data{
+				{UID: "3"},
+			},
+		},
+	}
+
+	sendBody, err := SendConsumptionReport(cdrServiceUrl, cdrReport)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	debug(string(sendBody))
+
+	cdrReport.Action = "ack"
+
+	ackBody, err := GetAcknowledgement(cdrServiceUrl, cdrReport)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	debug(string(ackBody))
+
+	var ackResp cdr.Request
+	err = json.Unmarshal(ackBody, &ackResp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, data := range ackResp.DataSet.Results {
+		// TODO
+		debug(string(data.Status))
+	}
+
 	debug("Done.")
 	os.Exit(0)
 }
